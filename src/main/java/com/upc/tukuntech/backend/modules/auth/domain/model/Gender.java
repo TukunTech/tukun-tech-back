29bd4240415564f61b84a0ecb9cea6fd17dc1502
Nicolini03 <U202218791@upc.edu.pe>
2025-10-24 00:32:51 -0500

    chore: add missing moved files from previous refactor commits

--- src/main/java/com/upc/tukuntech/backend/modules/auth/domain/model/Gender.java
+++
@@ -1,8 +0,0 @@
-package com.upc.tukuntech.backend.modules.auth.domain.model;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
-
--- src/main/java/com/upc/tukuntech/backend/modules/iam/application/dto/RefreshRequest.java
+++ src/main/java/com/upc/tukuntech/backend/modules/iam/application/dto/RefreshRequest.java
@@ -1,4 +1,4 @@
-package com.upc.tukuntech.backend.modules.auth.application.dto;
+package com.upc.tukuntech.backend.modules.iam.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
